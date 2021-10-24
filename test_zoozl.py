af84a7e7c284d713bee4bd7895e240c638413fd8
Juris <juris.kaminskis@gmail.com>
2021-10-24 16:34:29 +0300

    fix issue with paths

--- test_zoozl.py
+++ test_zoozl.py
@@ -2,6 +2,7 @@ import threading
 import time
 from signal import SIGINT
 import subprocess
+import os
 import unittest
 
 
@@ -10,28 +11,31 @@ class Main(unittest.TestCase):
 
     def test_zoozl(self):
         args = []
-        args.append("/home/juris/py-programs/zoozl/bin/python")
+        os.chdir("../")
+        args.append("bin/python")
         args.append("-u")
-        args.append("/home/juris/py-programs/zoozl/source/zoozl.py")
+        args.append("source/zoozl.py")
         proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         self.get_stream(proc)
         found = False
         for i in self.lines:
             line = i.split()
-            if "odoo.modules.registry:" in line[5] \
+            test = "odoo.modules.registry:"
+            if test in line \
                     and "Registry" in line[6]:
                 found = True
-        msg = f"Last line should contain {line[5]}"
-        self.assertTrue(found, msg=msg)
         proc.send_signal(SIGINT)
         proc.wait(timeout=4)
+        proc.poll()
+        msg = f"Last line should contain {test} in {line}"
+        self.assertTrue(found, msg=msg)
 
     def get_stream(self, proc):
         t = threading.Thread(target=self.read_items_from_proc, args=(proc,), daemon=True)
         t.start()
         a = t.is_alive()
         while a:
-            t.join(timeout=7)
+            t.join(timeout=8)
             a = t.is_alive()
             if a:
                 break
--- zoozl.py
+++ zoozl.py
@@ -3,6 +3,6 @@ import sys
 
 
 if __name__ == "__main__":
-    proc = subprocess.run(("../bin/python", "odoo/odoo-bin", "-dzoozl", "-rsanita", "-wblock", "-ubase"))
+    proc = subprocess.run(("bin/python", "source/odoo/odoo-bin", "-dzoozl", "-rsanita", "-wblock", "-ubase"))
     proc.check_returncode()
     sys.exit()
