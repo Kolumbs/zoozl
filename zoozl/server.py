1d4ddaa132856904908a3dfb9771808bc2cee05d
Juris <juris.kaminskis@gmail.com>
2022-10-16 13:36:34 +0300

    gracefully end connection reset errors

--- zoozl/server.py
+++ zoozl/server.py
@@ -39,35 +39,38 @@ class ZoozlBot(socketserver.StreamRequestHandler):
     """TCP server that listens on port for Zoozl boot calls"""
 
     def handle(self):
-        response = tcp_line(self.request) # Need to read request line for headers to read
-        if response[0] != b"GET":
-            log.info('Unrecognised message from %s: %s', self.client_address, response)
-            return
-        log.info("Client Connected: %s", self.client_address)
-        headers = http.client.parse_headers(self.rfile)
-        if "Sec-WebSocket-Key" not in headers:
-            sendback = b'HTTP/1.1 400 Missing Sec-WebSocket-Key header\r\n'
-            self.request.send(sendback)
-            return
-        self.request.send(websocket.handshake(headers["Sec-WebSocket-Key"]))
-        bot = chat.Chat(self.client_address, self.send_message)
-        bot.greet()
-        while True:
-            frame = websocket.read_frame(self.request)
-            if frame.op_code == "TEXT":
-                log.debug("Asking: %s", frame.data.decode())
-                msg = {}
-                try:
-                    msg = json.loads(frame.data.decode())
-                except json.decoder.JSONDecodeError:
-                    log.info("Invalid json format: %s", frame.data.decode())
-                if "text" in msg:
-                    bot.ask(chat.Message(msg["text"]))
-            elif frame.op_code == "CLOSE":
-                self.send_close(frame.data)
-                break
-            elif frame.op_code == "PING":
-                self.send_pong(frame.data)
+        try:
+            response = tcp_line(self.request) # Need to read request line for headers to read
+            if response[0] != b"GET":
+                log.info('Unrecognised message from %s: %s', self.client_address, response)
+                return
+            log.info("Client Connected: %s", self.client_address)
+            headers = http.client.parse_headers(self.rfile)
+            if "Sec-WebSocket-Key" not in headers:
+                sendback = b'HTTP/1.1 400 Missing Sec-WebSocket-Key header\r\n'
+                self.request.send(sendback)
+                return
+            self.request.send(websocket.handshake(headers["Sec-WebSocket-Key"]))
+            bot = chat.Chat(self.client_address, self.send_message)
+            bot.greet()
+            while True:
+                frame = websocket.read_frame(self.request)
+                if frame.op_code == "TEXT":
+                    log.debug("Asking: %s", frame.data.decode())
+                    msg = {}
+                    try:
+                        msg = json.loads(frame.data.decode())
+                    except json.decoder.JSONDecodeError:
+                        log.info("Invalid json format: %s", frame.data.decode())
+                    if "text" in msg:
+                        bot.ask(chat.Message(msg["text"]))
+                elif frame.op_code == "CLOSE":
+                    self.send_close(frame.data)
+                    break
+                elif frame.op_code == "PING":
+                    self.send_pong(frame.data)
+        except ConnectionResetError:
+            log.info("Client % dropped connection", self.client_address)
 
     def send_close(self, text):
         """send close frame"""
