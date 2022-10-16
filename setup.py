80873253b9b14b07566819977eb62ccd289e4e97
Juris <juris.kaminskis@gmail.com>
2022-10-16 08:20:42 +0300

    add zoozl.chat package to wheel

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
     setuptools.setup(
         name="zoozl",
-        version="0.0.2",
+        version="0.0.3",
         author="Juris Kaminskis",
         author_email="juris@zoozl.net",
         description="Zoozl services",
         long_description=long_description,
         long_description_content_type="text/markdown",
         url="https://github.com/Kolumbs/zoozl",
-        packages=["zoozl"],
+        packages=["zoozl", "zoozl.chat"],
         install_requires=[
             "rapidfuzz==2.11.1",
             "membank==0.4.0",
